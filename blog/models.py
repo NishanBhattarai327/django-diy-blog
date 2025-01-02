05c34692291288b3f4f38a4cb9e19303afb3527b
NishanBhattarai327 <bhattarainishan327@gmail.com>
2025-01-02 08:30:20 +0545

    Change order of Blog model by Newest to oldest (Complete all blog page requirement)

--- blog/models.py
+++ blog/models.py
@@ -13,7 +13,7 @@ class Blog(models.Model):
     blogger = models.ForeignKey('Blogger', on_delete=models.RESTRICT, null=True)
 
     class Meta:
-        ordering = ['post_date']
+        ordering = ['-post_date']
 
     def __str__(self):
         return self.name
